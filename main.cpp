0922cc80d0ec54758e832418253d63a73ccdfbd4
abubakar471 <abdurjoy2001@gmail.com>
2026-04-30 03:12:43 +0600

    finalized the project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
+#include "./ab/ab.cpp"
 #include "./mushfiq/mushfiq.cpp"
+#include "./azwad/azwad.cpp"
+#include "./Rk/Rk.cpp"
+#include "./Rakib/rakib.cpp"
+
 using namespace std;
 
 int main() {
-    string engineers[] = {AB::show()};
+    string engineers[] = {AB::show(), mushfiq::show(), RAKIB::show(), azwad::show(), RK::show()};
 
     for(auto people : engineers){
         cout << people << endl;
